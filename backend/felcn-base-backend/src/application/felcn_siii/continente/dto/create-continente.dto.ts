6268c92563785b7f85c6c4b727746e446c413b72
alcida.chambi <alcida.chambi@produccion.gob.bo>
2026-03-03 06:28:05 -0400

    migracion continente

--- backend/felcn-base-backend/src/application/felcn_siii/continente/dto/create-continente.dto.ts
+++ backend/felcn-base-backend/src/application/felcn_siii/continente/dto/create-continente.dto.ts
@@ -1,1 +1,14 @@
-export class CreateContinenteDto {}
+import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator'
+
+export class CreateContinenteDto {
+  @ApiProperty({
+    example: 'Sudamérica',
+    description: 'Nombre oficial del continente',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(150)
+  descripcion: string
+}
+
