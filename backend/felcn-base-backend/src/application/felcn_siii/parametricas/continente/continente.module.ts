f0bffa3f668387bcbd4d3de3ad9d99a93acd60e6
alcishujey11@gmail.com <alcishujey11@gmail.com>
2026-03-06 16:48:13 -0400

    generador de pdf y excel, ordenamiento

--- backend/felcn-base-backend/src/application/felcn_siii/parametricas/continente/continente.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ContinenteService } from './continente.service';
-import { ContinenteController } from './continente.controller';
-
-@Module({
-  controllers: [ContinenteController],
-  providers: [ContinenteService],
-})
-export class ContinenteModule {}
