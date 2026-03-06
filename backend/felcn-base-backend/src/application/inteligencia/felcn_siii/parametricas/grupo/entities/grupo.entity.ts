f574569b369a3e47ace5981cc13916f8c66a24c6
alcishujey11@gmail.com <alcishujey11@gmail.com>
2026-03-06 16:48:13 -0400

    generador de pdf y excel, ordenamiento

---
+++ backend/felcn-base-backend/src/application/inteligencia/felcn_siii/parametricas/grupo/entities/grupo.entity.ts
@@ -0,0 +1,66 @@
+import {
+  BeforeInsert,
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm'
+import { Distrital } from '../../distrital/entities/distrital.entity'
+import { Usuario } from '../../usuario/entities/usuario.entity'
+import { Estado } from '../../../estado.enum'
+import { Asignacion } from '../../../operaciones/asignaciones/entities/asignacione.entity'
+
+
+@Entity({ name: 'grupo' })
+@Index('UQ_grupo_distrito_descripcion', ['distrital', 'descripcion'], {
+  unique: true,
+})
+export class Grupo {
+  @PrimaryGeneratedColumn({
+    type: 'int',
+    name: 'id_grupo',
+    comment: 'Clave primaria del registro',
+  })
+  idGrupo: number
+
+  @ManyToOne(() => Distrital, {
+    nullable: false,
+    onDelete: 'RESTRICT',
+  })
+  @JoinColumn({
+    name: 'id_distrital',
+  })
+  distrital: Distrital
+
+  @Column({
+    type: 'varchar',
+    length: 150,
+    nullable: false,
+    comment: 'Descripción del grupo',
+  })
+  descripcion: string
+
+  @Column({
+    type: 'enum',
+    enum: Estado,
+    default: Estado.ACTIVO,
+    comment: 'Estado del registro',
+  })
+  estado: Estado
+
+  @BeforeInsert()
+  setEstadoPorDefecto() {
+    if (!this.estado) {
+      this.estado = Estado.ACTIVO
+    }
+  }
+
+  @OneToMany(() => Asignacion, (asignacion) => asignacion.grupo)
+  asignaciones: Asignacion[]
+
+  @OneToMany(() => Usuario, (usuario) => usuario.grupo)
+  usuarios: Usuario[]
+}
---
+++ backend/felcn-base-backend/src/application/inteligencia/felcn_siii/parametricas/pais/entities/pais.entity.ts
@@ -0,0 +1,60 @@
+import {
+  BeforeInsert,
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm'
+import { Continente } from '../../continente/entities/continente.entity'
+import { Departamento } from '../../departamento/entities/departamento.entity'
+import { Estado } from '../../../estado.enum'
+
+@Entity({
+  name: 'pais',
+  schema: 'parametricas',
+})
+export class Pais {
+  @PrimaryGeneratedColumn({
+    type: 'int',
+    name: 'id_pais',
+    comment: 'Clave primaria del registro',
+  })
+  idPais: number
+
+  @Index({ unique: true })
+  @Column({
+    type: 'varchar',
+    length: 150,
+    nullable: false,
+    comment: 'Nombre oficial del país',
+  })
+  descripcion: string
+
+  @ManyToOne(() => Continente, (continente) => continente.paises, {
+    nullable: false,
+    onDelete: 'RESTRICT',
+  })
+  @JoinColumn({ name: 'id_continente' })
+  continente: Continente
+
+  @Column({
+    type: 'enum',
+    enum: Estado,
+    default: Estado.ACTIVO,
+    comment: 'Estado del registro',
+  })
+  estado: Estado
+
+  @BeforeInsert()
+  setEstadoPorDefecto() {
+    if (!this.estado) {
+      this.estado = Estado.ACTIVO
+    }
+  }
+
+  @OneToMany(() => Departamento, (departamento) => departamento.pais)
+  departamentos: Departamento[]
+}
