9900400556f2d0cf15dd2d366bc460be8cf3c779
toribiov <toribiov@sintesis.com.bo>
2026-03-15 14:57:29 -0400

    feat: nerge

--- frontend/felcn-base-frontend/src/app/operaciones/operativo/gestion-operativo/registro/ui/FormGestionOperativo.tsx
+++ frontend/felcn-base-frontend/src/app/operaciones/operativo/gestion-operativo/registro/ui/FormGestionOperativo.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useState } from 'react'
+
 import { DatosGeneralesForm } from './secciones/DatosGeneralesForm'
 import { SeccionDrogasFotografiaLogotiposForm } from './secciones/SeccionDrogasFotografiaLogotiposForm'
 import { SustanciasSolidas } from './secciones/SustanciasSolidas'
