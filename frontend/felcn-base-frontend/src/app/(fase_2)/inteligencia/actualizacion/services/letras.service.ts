af3bd65060b890de3592b774b764b4c5a4b7459c
Alcida <alcishujey11@gmail.com>
2026-03-17 14:23:43 -0400

    Add letras service

--- frontend/felcn-base-frontend/src/app/(fase_2)/inteligencia/actualizacion/services/letras.service.ts
+++ frontend/felcn-base-frontend/src/app/(fase_2)/inteligencia/actualizacion/services/letras.service.ts
@@ -2,24 +2,16 @@ import { Constantes } from '@/config/Constantes'
 import { usePeticion } from '@/hooks'
 
 export interface LetraInicial {
-  idLetra: number
-  letra: string
+  descripcion: string
 }
 
 const { sesionPeticion } = usePeticion()
 
 export async function getIniciales(): Promise<LetraInicial[]> {
-  //   const response = await sesionPeticion({
-  //     url: `${Constantes.baseUrl}/departamento/all/pais`,
-  //     withCredentials: true,
-  //   })
+  const response = await sesionPeticion({
+    url: `${Constantes.baseUrl}/letra/allGeneral`,
+    withCredentials: true,
+  })
 
-  //   return response
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-  return [
-    { idLetra: 1, letra: 'A' },
-    { idLetra: 2, letra: 'B' },
-    { idLetra: 3, letra: 'C' },
-    { idLetra: 4, letra: 'D' },
-  ]
+  return response
 }
