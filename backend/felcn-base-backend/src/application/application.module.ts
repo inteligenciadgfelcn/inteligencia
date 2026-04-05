e1ad682cb62aa3de6154ad11d5663d72e6f08d63
Alcida <alcishujey11@gmail.com>
2026-04-05 11:49:43 -0400

    add interoperabilidad

--- backend/felcn-base-backend/src/application/application.module.ts
+++ backend/felcn-base-backend/src/application/application.module.ts
@@ -3,8 +3,14 @@ import { ParametroModule } from './parametro/parametro.module'
 import { HealthModule } from './health/health.module'
 // import { SunesisModule } from './sunesis/sunesis.module'
 import { InteligenciaModule } from './inteligencia/inteligencia.module'
+import { InteroperabilidadModule } from './interoperabilidad/interoperabilidad.module'
 
 @Module({
-  imports: [ParametroModule, HealthModule, InteligenciaModule],
+  imports: [
+    ParametroModule,
+    HealthModule,
+    InteligenciaModule,
+    InteroperabilidadModule,
+  ],
 })
 export class ApplicationModule { }
