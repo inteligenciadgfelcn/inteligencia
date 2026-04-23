10514fdc45c4453d2145634910e9fbbb439c3e59
Eitner Montero <eitnermontero@gmail.com>
2026-04-23 12:22:52 -0400

    fix: add libreria chrome-headless-shell v2

--- backend/felcn-base-backend-v2/src/application/inteligencia/reportes/export/pdf/pdf.service.ts
+++ backend/felcn-base-backend-v2/src/application/inteligencia/reportes/export/pdf/pdf.service.ts
@@ -37,7 +37,8 @@ export class PdfService {
     // lanzar navegador
     const browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium-browser',
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
     });
 
     const page = await browser.newPage();
