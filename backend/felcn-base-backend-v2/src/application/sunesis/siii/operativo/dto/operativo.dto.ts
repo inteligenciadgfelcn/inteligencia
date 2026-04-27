c52c88caeaeca188320de9e2f55589a1be6aefec
emontero <Informatica1352>
2026-04-26 22:55:08 -0400

    feat: correcion de nombre de campo a numeroInformeç

--- backend/felcn-base-backend-v2/src/application/sunesis/siii/operativo/dto/operativo.dto.ts
+++ backend/felcn-base-backend-v2/src/application/sunesis/siii/operativo/dto/operativo.dto.ts
@@ -15,11 +15,11 @@ import {
 export class OperativoDto {
   @ApiProperty({
     description: 'Número de informe del operativo (ingresado por el usuario)',
-    example: 'IC-042/2026',
+    example: 'INF-042/2026',
   })
   @IsNotEmpty()
   @IsString()
-  @MaxLength(20)
+  @MaxLength(30)
   numeroInforme: string
 
   @ApiProperty({ description: 'ID tipo de relevancia', example: 1 })
