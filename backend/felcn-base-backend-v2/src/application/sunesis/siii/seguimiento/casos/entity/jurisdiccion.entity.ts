051a1a4ef57dca3b218c0600220825ea2ee19153
erikacamargo936 <erikacamargo936@gmail.com>
2026-05-12 15:45:03 -0400

    feat: se traslada seguimiento casos a una carpeta

--- backend/felcn-base-backend-v2/src/application/sunesis/siii/seguimiento/casos/entity/jurisdiccion.entity.ts
+++ backend/felcn-base-backend-v2/src/application/sunesis/siii/seguimiento/casos/entity/jurisdiccion.entity.ts
@@ -1,31 +1,28 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm'
-import { AsignacionSiii } from '../../asignacion/entity/asignacion-siii.entity'
-import { SCHEMA_PUBLIC } from '../../../shared/constants'
+import { AsignacionSiii } from '../../../asignacion/entity/asignacion-siii.entity'
+import { SCHEMA_PUBLIC } from '../../../../shared/constants'
 
 /**
- * Entidad Fiscal
- * Representa el historial de fiscales asignados a un caso.
- * Tabla: fiscal
+ * Entidad Jurisdiccion
+ * Historial de las jurisdicciones por las que ha pasado un caso.
+ * Tabla: jurisdiccion
  */
-@Entity({ name: 'fiscal', schema: SCHEMA_PUBLIC })
-export class Fiscal {
-  @PrimaryGeneratedColumn({ type: 'bigint', name: 'id_fiscal' })
+@Entity({ name: 'jurisdiccion', schema: SCHEMA_PUBLIC })
+export class Jurisdiccion {
+  @PrimaryGeneratedColumn({ type: 'bigint', name: 'id_jurisdiccion' })
   id: string
 
   @Column({ name: 'id_caso', type: 'bigint' })
   idCaso: string
 
-  @Column({ name: 'nombre_apellidos', type: 'varchar', length: 150 })
-  nombreApellidos: string
-
-  @Column({ name: 'telefono_celular', type: 'varchar', length: 10 })
-  telefonoCelular: string
-
-  @Column({ name: 'telefono_fijo', type: 'varchar', length: 10 })
-  telefonoFijo: string
-
   @Column({ name: 'fecha', type: 'timestamp' })
   fecha: Date
+
+  @Column({ name: 'jurisdiccion', type: 'varchar', length: 100 })
+  jurisdiccion: string
+
+  @Column({ name: 'observacion', type: 'varchar', length: 150 })
+  observacion: string
 
   @Column({ name: 'es_actual', type: 'bool' })
   esActual: boolean
@@ -43,7 +40,7 @@ export class Fiscal {
   @JoinColumn({ name: 'id_caso' })
   asignacion: AsignacionSiii
 
-  constructor(data?: Partial<Fiscal>) {
+  constructor(data?: Partial<Jurisdiccion>) {
     if (data) Object.assign(this, data)
   }
 }
