a18c16e34772cdf650787209c17d4817dccb5c49
alcida.chambi <alcida.chambi@produccion.gob.bo>
2026-07-01 06:51:30 -0400

    configuracion swagger

--- backend/felcn-base-backend-v2/src/application/catalogo-fiscalia/catalogo-fiscalia.controller.ts
+++ backend/felcn-base-backend-v2/src/application/catalogo-fiscalia/catalogo-fiscalia.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UnidadLgiService } from '../lgi/parametro/unidad/unidad.service';
+import { CreateUnidadDto } from '../lgi/parametro/unidad/dto/create-unidad.dto';
 
 @Controller('catalogo-fiscalia')
-export class CatalogoFiscaliaController {}
+export class CatalogoFiscaliaController {
+     constructor(
+        private readonly unidadService: UnidadLgiService,
+      ) {}
+    
+     @Post()
+      create(@Body() dto: CreateUnidadDto) {
+        return this.unidadService.create(dto)
+      }
+}
--- backend/felcn-base-backend-v2/src/application/catalogo-fiscalia/catalogo-fiscalia.module.ts
+++ backend/felcn-base-backend-v2/src/application/catalogo-fiscalia/catalogo-fiscalia.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { CatalogoFiscaliaController } from './catalogo-fiscalia.controller';
 
 @Module({
+  
   controllers: [CatalogoFiscaliaController]
 })
 export class CatalogoFiscaliaModule {}
