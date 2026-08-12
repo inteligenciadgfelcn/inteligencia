42d92fc06a54fbc5b9e534edcfbff54bb521dd9c
alcida.chambi <alcida.chambi@produccion.gob.bo>
2026-08-11 22:11:35 -0400

    datos generales asignacion

--- backend/felcn-base-backend-v2/src/application/lgi/asignacion_lgi/asignacion_lgi.controller.ts
+++ backend/felcn-base-backend-v2/src/application/lgi/asignacion_lgi/asignacion_lgi.controller.ts
@@ -1,4 +1,16 @@
-import {Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Query} from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Query,
+  UseInterceptors,
+  ParseIntPipe,
+} from '@nestjs/common'
 import { AsignacionLgiService } from './asignacion_lgi.service'
 import { CreateAsignacionLgiDto } from './dto/create-asignacion_lgi.dto'
 import { UpdateAsignacionLgiDto } from './dto/update-asignacion_lgi.dto'
@@ -6,11 +18,13 @@ import { JwtAuthGuard } from '@/core/config/authorization/guards/jwt-auth.guard'
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger'
 import { BaseController } from '@/common/base/base-controller'
 import { PaginacionQueryDto } from '@/common/dto/paginacion-query.dto'
+import { AuditoriaUsuarioInterceptor } from '@/common/interceptors/auditoria-usuario.interceptor'
 import { CrearNumeroCasoDto } from '@/application/inteligencia/felcn_asignacion_caso/asignaciones/dto/create_numeroCaso.dto'
 import { AsignacionesService } from '@/application/inteligencia/felcn_asignacion_caso/asignaciones/asignaciones.service'
 
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard)
+@UseInterceptors(AuditoriaUsuarioInterceptor)
 @ApiTags('LGI - Asignación')
 @Controller('asignacion-lgi')
 export class AsignacionLgiController extends BaseController {
@@ -21,9 +35,22 @@ export class AsignacionLgiController extends BaseController {
     super()
   }
 
-  @Post()
+  @Post('crear-datosGenerales')
+  @ApiOperation({ summary: 'Crear asignacion seccion datos generales' })
   create(@Body() createAsignacionLgiDto: CreateAsignacionLgiDto) {
     return this.asignacionLgiService.create(createAsignacionLgiDto)
+  }
+
+  @Patch(':idAsignacion')
+  @ApiOperation({
+    summary: 'Actualizar los datos generales de una asignación',
+  })
+  updateDatosGenerales(
+    @Param('idAsignacion')
+    id: number,
+    @Body() dto: UpdateAsignacionLgiDto
+  ) {
+    return this.asignacionLgiService.update(id, dto)
   }
 
   @Get()
@@ -34,10 +61,10 @@ export class AsignacionLgiController extends BaseController {
   }
 
   @Post('generar-numero')
-    @ApiOperation({ summary: 'Generar número de caso' })
-    generar(@Body() dto: CrearNumeroCasoDto) {
-      return this.service.generarNumeroCaso(dto.codigoDepartamento, dto.letra)
-    }
+  @ApiOperation({ summary: 'Generar número de caso' })
+  generar(@Body() dto: CrearNumeroCasoDto) {
+    return this.service.generarNumeroCaso(dto.codigoDepartamento, dto.letra)
+  }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
