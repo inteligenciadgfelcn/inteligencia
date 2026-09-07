853696d2b9b63895d8fa0d336a43b0072d060bf2
alcida.chambi <alcida.chambi@produccion.gob.bo>
2026-09-06 23:32:11 -0400

    reporte de operaciones inteligencia

--- backend/felcn-base-backend/src/application/inteligencia/reportes/reporte/reporte.module.ts
+++ backend/felcn-base-backend/src/application/inteligencia/reportes/reporte/reporte.module.ts
@@ -1,23 +1,17 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { ExportModule } from "../export/export.module";
-import { Detenido } from "../../felcn_sii/filiacion/detenido/entities/detenido.entity";
-import { DB_SII } from "@/core/config/database/database.module";
-import { Huella } from "../../felcn_sii/huella/entities/huella.entity";
-import { ReporteController } from "./reporte.controller";
-import { ReporteService } from "./reporte.service";
-import { ReporteServicioRepository } from "./repository/reporte_servicio.repository";
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ExportModule } from '../export/export.module'
+import { Detenido } from '../../felcn_sii/filiacion/detenido/entities/detenido.entity'
+import { DB_SII } from '@/core/config/database/database.module'
+import { Huella } from '../../felcn_sii/huella/entities/huella.entity'
+import { ReporteController } from './reporte.controller'
+import { ReporteService } from './reporte.service'
+import { ReporteServicioRepository } from './repository/reporte_servicio.repository'
 
 @Module({
-    imports: [
-    TypeOrmModule.forFeature([Detenido, Huella],DB_SII), 
-    ExportModule,
-  ],
+  imports: [TypeOrmModule.forFeature([Detenido, Huella], DB_SII), ExportModule],
   controllers: [ReporteController],
   providers: [ReporteService, ReporteServicioRepository],
-   exports: [
-    ReporteService,
-    ReporteServicioRepository,
-  ],
+  exports: [ReporteService, ReporteServicioRepository],
 })
 export class ReporteModule {}
