7a41ce8ec76095bdf186ddea7660572a00398814
alimachi.dev <alimachi.dev@gmail.com>
2026-09-18 06:23:52 -0400

    Fix show reports in prod

--- backend/felcn-base-backend/src/application/inteligencia/reportes/images.ts
+++ backend/felcn-base-backend/src/application/inteligencia/reportes/images.ts
@@ -1,17 +1,8 @@
 import * as path from 'path'
 
 export const pdfImages = {
-  institucional: path.resolve(
-    process.cwd(),
-    'src/application/inteligencia/reportes/assets/images/felcnlogo.png'
-  ),
-  escudo: path.resolve(
-    process.cwd(),
-    'src/application/inteligencia/reportes/assets/images/escudo.png'
-  ),
+  institucional: path.resolve(__dirname, 'assets/images/felcnlogo.png'),
+  escudo: path.resolve(__dirname, 'assets/images/escudo.png'),
 
-  marcaAgua: path.resolve(
-    process.cwd(),
-    'src/application/inteligencia/reportes/assets/images/marca-agua.png'
-  ),
+  marcaAgua: path.resolve(__dirname, 'assets/images/marca-agua.png'),
 }
